bf432f426c47ee8ea85cbe7e3114a27ceb3ce8a4
matheus <matheusrafael@ufba.br>
2025-11-24 13:07:40 -0300

    att

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ git submodule update --init --recursive
 sudo apt-get update
 rosdep update --rosdistro=$ROS_DISTRO
 rosdep install --from-paths src --ignore-src -r -i -y --rosdistro=$ROS_DISTRO
+
+
+
